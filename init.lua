e3121c7de693ef5bab78697f2cee26ad995309f6
narcis <narcisstaicu@icloud.com>
2026-08-17 04:35:28 +0100

    change theme to dracula

--- init.lua
+++ init.lua
@@ -404,18 +404,26 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { gh 'Mofiqul/dracula.nvim' }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
-    styles = {
-      comments = { italic = false }, -- Disable italics in comments
-    },
+  require('dracula').setup {
+    -- customize dracula color palette
+    -- colors = { ... },
+
+    -- show the '~' characters after the end of buffers
+    show_end_of_buffer = false,
+    -- use transparent background
+    transparent_bg = false,
+    -- set italic comment
+    italic_comment = false,
+    -- overrides the default highlights
+    overrides = {},
   }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'dracula'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
