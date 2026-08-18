a00b0430b488593859ca7853666c299508fdd880
narcis <narcisstaicu@icloud.com>
2026-08-18 23:15:20 +0100

    make tabs behave as 4 spaces on new files

--- init.lua
+++ init.lua
@@ -171,6 +171,11 @@ do
   -- instead raise a dialog asking if you wish to save the current file(s)
   -- See `:help 'confirm'`
   vim.o.confirm = true
+
+  vim.o.tabstop = 4
+  vim.o.shiftwidth = 4
+  vim.o.softtabstop = 4
+  vim.o.expandtab = true
 end
 
 -- ============================================================
